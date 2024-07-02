49f19ad63d79d38fce4fc81a94d20a715c231938
Bruno Gomes <bsgomes16@gmail.com>
2024-07-02 17:32:11 -0300

    function to do validation was finished

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 //HTML elements and Values as variables
-const textarea = document.querySelector('#textInput');
+const textarea = document.querySelector("#textInput");
 
-//
+//Function which will be called to encrypt the user text
 function toEncryptTheUserText() {
-  let textToBeEncrypted = textarea.value.trim().toLowerCase();
+  let textToBeValidated = textarea.value;
+  const textNormalizated = validateText(textToBeValidated);
 
-  if (textToBeEncrypted.length < 5) {
-    alert('Por favor insira um texto válido')
-  }
-  console.log(textToBeEncrypted)
+  console.log(textNormalizated);
 }
 
+//Function which will be called to validate the user text, changing the strangers characters
 function validateText(textToBeValidated) {
+  const textInValidator = textToBeValidated
+    .trim()
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
 
+  if (textInValidator.length < 5) {
+    alert("Por favor insira um texto válido");
+    textarea.focus();
+    return;
+  }
+
+
+  return textInValidator;
 }
-
